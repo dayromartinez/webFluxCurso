a7fcdb8b19c9e70e44f87c499590c134db349afb
Dayro Martínez <dayro.martinez@sofka.com.co>
2022-08-12 15:42:57 -0500

    Creación del proyecto y del endpoint

---
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/sofka/api/Handler.java
@@ -0,0 +1,18 @@
+package co.com.sofka.api;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+@Component
+@RequiredArgsConstructor
+public class Handler {
+
+    public Mono<ServerResponse> listenGETUseCase(ServerRequest serverRequest) {
+
+        return ServerResponse.ok().bodyValue("");
+    }
+
+}
